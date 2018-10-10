d9ecd38919067654feb6f63730488917513f5956
robertohogue <36170635+robertohogue@users.noreply.github.com>
2018-10-10 15:16:06 -0700

    printPowersOfN

---
+++ Power.java
@@ -0,0 +1,15 @@
+
+public class Power {
+
+    public static void main(String[] args) {
+    	printPowersOfN(2,7);
+    	printPowersOfN(5,3);
+    }	
+    	public static void printPowersOfN(int num1, int count) {
+    		for(int i = 1; i <=count; i++) {
+    			double result= Math.pow(num1, i);
+    			System.out.println(result);
+    		}
+    	}
+ 
+}
